a77531c694a39e24ef2a6317f29d70084ce71cb7
Martin-bot-man <martinwere14@gmail.com>
2025-11-23 08:35:29 +0300

    feat: created the first models in models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class user(models.Model):
+
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=254, unique=True)
+    password_hash = models.CharField(max_length=128)
+    phone_number = models.IntegerField(blank=True, null=False,unique=True)
 
 # Create your models here.
